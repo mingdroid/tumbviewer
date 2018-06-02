4e3a923e266ffff04c086a328123169aa962ebac
huang <kenhm808@gmail.com>
2018-06-02 16:39:25 +0800

    fix some bug, update some lib

--- app/src/main/java/com/nutrition/express/downloadservice/ProgressListener.java
+++
@@ -1,9 +0,0 @@
-package com.nutrition.express.downloadservice;
-
-/**
- * Created by huang on 4/15/17.
- */
-
-interface ProgressListener {
-    void update(long bytesRead, long contentLength, boolean done);
-}
